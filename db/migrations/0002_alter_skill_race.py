dabd5039415ed7c875ffb6c53b64b73a794a8709
turulko-oleksandr <SashaTurulko@i.ua>
2025-07-09 12:29:55 +0300

    Solution

--- db/migrations/0002_alter_skill_race.py
+++ db/migrations/0002_alter_skill_race.py
@@ -14,6 +14,6 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='skill',
             name='race',
-            field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, to='db.race'),
+            field=models.ForeignKey(null=False, on_delete=django.db.models.deletion.CASCADE, to='db.race'),
         ),
     ]
