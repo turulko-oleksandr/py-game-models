546474d3c0b77043b56a8f8b13b4e15f326c37c4
turulko-oleksandr <SashaTurulko@i.ua>
2025-07-09 11:49:55 +0300

    Solution

--- main.py
+++ main.py
@@ -1,10 +1,49 @@
+import json
+
 import init_django_orm  # noqa: F401
 
 from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
-    pass
+    with open("players.json", encoding="utf-8") as f:
+        players_data = json.load(f)
+
+    for players_name, player_data in players_data.items():
+        # Race
+        race_data = player_data["race"]
+        race_obj, _ = Race.objects.get_or_create(
+            name=race_data["name"],
+            defaults={"description": race_data.get("description", "")}
+        )
+
+        # Skills
+        for skill_data in race_data.get("skills", []):
+            Skill.objects.get_or_create(
+                name=skill_data["name"],
+                race=race_obj,
+                defaults={"bonus": skill_data["bonus"]}
+            )
+
+        # Guild
+        guild_obj = None
+        if player_data.get("guild") is not None:
+            guild_data = player_data["guild"]
+            guild_obj, _ = Guild.objects.get_or_create(
+                name=guild_data["name"],
+                defaults={"description": guild_data.get("description")}
+            )
+
+        # Player
+        Player.objects.get_or_create(
+            nickname=players_name,
+            defaults={
+                "email": player_data["email"],
+                "bio": player_data["bio"],
+                "race": race_obj,
+                "guild": guild_obj,
+            }
+        )
 
 
 if __name__ == "__main__":
